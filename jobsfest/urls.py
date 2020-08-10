fb97cb3b8ae6a579ef6b6c81e732e14d6acc0297
amit <amitsaini@sswa.in>
2020-08-10 10:31:17 +0530

    assd

--- jobsfest/urls.py
+++ jobsfest/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home),
     path('copening/',views.jopening),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
